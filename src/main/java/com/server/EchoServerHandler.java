fc02e7bc503e8ada5e6e43db518154d51c2182a3
lileibh <lileibh@gmail.com>
2017-07-21 19:33:47 +0800

    init project

---
+++ src/main/java/com/server/EchoServerHandler.java
@@ -0,0 +1,36 @@
+package com.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * Created by lei on 20/07/2017.
+ * you can you up
+ */
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println(buf.toString(CharsetUtil.UTF_8));
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/server/HelloNetty.java
@@ -0,0 +1,39 @@
+package com.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by lei on 20/07/2017.
+ * you can you up
+ */
+public class HelloNetty {
+
+    public static void main(String[] args) throws Exception {
+        final EchoServerHandler handler = new EchoServerHandler();
+        EventLoopGroup loopGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap sbs = new ServerBootstrap();
+            sbs.group(loopGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(9191))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(handler);
+                        }
+                    });
+            ChannelFuture future = sbs.bind().sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            loopGroup.shutdownGracefully().sync();
+        }
+    }
+}
